c5f6c112b8523718acca0abfa25da43acca40215
MaikGon <michal.gontarczyk@wp.pl>
2021-05-03 16:11:14 +0200

    Line follow changes

--- Tello_Video/line_follower.py
+++ Tello_Video/line_follower.py
@@ -1,7 +1,5 @@
 import cv2
 import numpy as np
-import time
-import socket
 from djitellopy import tello
 
 
@@ -9,8 +7,16 @@ me = tello.Tello()
 me.connect()
 print(me.get_battery())
 me.streamon()
+# me.takeoff()
 
 hsv_Vals = [0, 0, 158, 179, 255, 255]
+num_sensors = 3
+th_value = 0.3
+width, height = 480, 360
+sensitivity = 3
+weights = [-25, -15, 0, 15, 25]
+curve = 0
+fSpeed = 10
 
 
 def thresholding(img):
@@ -24,18 +30,81 @@ def thresholding(img):
 
 def contours(img_th, img):
     cnt, _ = cv2.findContours(img_th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
-    cv2.drawContours(img, cnt, -1, (255, 255, 0), 7)
+    cx = 0
+    try:
+        biggest = max(cnt, key=cv2.contourArea)
+        x, y, w, h = cv2.boundingRect(biggest)
+        cx = x + w//2
+        cy = y + h//2
+        cv2.drawContours(img, biggest, -1, (255, 255, 0), 7)
+        cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
+
+    except:
+        pass
+    return cx
+
+
+def getSensorOut(imgThresh, num_sensors):
+    imgs = np.hsplit(imgThresh, num_sensors)
+    pixels = (imgThresh.shape[1]//num_sensors) * imgThresh.shape[0]
+    sensor_out = []
+    for num, im in enumerate(imgs):
+        cnt_pixels = cv2.countNonZero(im)
+        if cnt_pixels > th_value*pixels:
+            sensor_out.append(1)
+        else:
+            sensor_out.append(0)
+        cv2.imshow(str(num), im)
+    print(sensor_out)
+
+    return sensor_out
+
+
+def send_commands(sensor_out, cx):
+    global curve
+    # Translation
+    lr = (cx - width//2) // sensitivity
+    lr = int(np.clip(lr, -10, 10))
+
+    if 2 > lr > -2:
+        lr = 0
+
+    # Rotation
+    if sensor_out == [1, 0, 0]:
+        curve = weights[0]
+    elif sensor_out == [1, 1, 0]:
+        curve = weights[1]
+    elif sensor_out == [0, 1, 0]:
+        curve = weights[2]
+    elif sensor_out == [0, 1, 1]:
+        curve = weights[3]
+    elif sensor_out == [0, 0, 1]:
+        curve = weights[4]
+    elif sensor_out == [0, 0, 0]:
+        curve = weights[2]
+    elif sensor_out == [1, 1, 1]:
+        curve = weights[2]
+    elif sensor_out == [1, 0, 1]:
+        curve = weights[2]
+    print(lr, fSpeed, 0, curve)
+    # me.send_rc_control(lr, fSpeed, 0, curve)
 
 
 while True:
     cap = me.get_frame_read().frame
-    img = cv2.resize(cap, (480, 360))
-    # img = cv2.flip(img, 0)
+    img = cv2.resize(cap, (width, height))
+    img = cv2.flip(img, 0)
 
     imgThresh = thresholding(img)
+    cx = contours(imgThresh, img)
+
+    sensor_out = getSensorOut(imgThresh, num_sensors)
+    send_commands(sensor_out, cx)
 
     cv2.imshow('Output', img)
     cv2.imshow('Path', imgThresh)
-    cv2.waitKey(1)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+cv2.destroyAllWindows()
 
 
---
+++ Tello_Video/test.py
@@ -0,0 +1,16 @@
+import cv2
+import numpy as np
+from djitellopy import tello
+import time
+
+me = tello.Tello()
+me.connect()
+print(me.get_battery())
+# me.streamon()
+me.takeoff()
+time.sleep(3)
+me.send_rc_control(0, 50, 0, 0)
+time.sleep(2)
+me.send_rc_control(0, 0, 0, 0)
+me.land()
+
