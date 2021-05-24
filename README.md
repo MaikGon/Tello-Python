35f940ab614d3cf13947cd4745e62d55e23c35e1
MaikGon <michal.gontarczyk@wp.pl>
2021-05-24 21:01:31 +0200

    Readme and comments

--- README.md
+++ README.md
@@ -1,44 +1,42 @@
-# Tello-Python
+# Tello Line Follower
 
-## Introduction
+Projekt na zajęcia z Autonomicznych Robotów Latających prowadzonych na Politechnice Poznańskiej.
 
-This is a collection of python-based sample code that interact with the Ryze Tello drone.
+## Opis projektu
 
-## Project Description
+W folderze 'Tello_Video' znajduje się plik line_follower.py, w którym zawarte zostały funkcje pozwalające na autonomiczne podążanie drona Tello po wyznaczonej trasie na ścianie danego pomieszczenia. Założeniem projektowym jest to, aby dron leciał zawsze w prawo.
 
-This toolkit contains three sample programs based on tello sdk and python2.7,including Single_Tello_Test, Tello_Video, and Tello_Video (With_Pose_Recognition). There is also a program file named tello_state.py.
 
-- **Single_Tello_Test**
+## Funkcje
 
- In Single_Tello_Test,You can design a series of command combinations by writing a txt script to let tello execute a series of actions you have designed. This program can also be used as a command set test tool for tello.
+### threshold
+Odpowiada za prawidłowe sprogowanie ścieżki, po które ma poruszać się dron. Ściężka powinna mieć kolor czerwony.
 
-- **Tello_Video**
+### contours
+Odpowiada za wykrywanie konturów znaczników w postaci kwadratowych kartek, aby móc sterować odległością od ściany.
 
- In Tello_Video，You can receive the video stream data from tello, decode the video through the h264 decoding library, and display it on a GUI interface based on Tkinter and PIL. In addition, it also supports a control panel that can operate tello. This sample code provides an example of receiving and processing and getting the correct video data. The source code of the h264 decoding library is also provided in the package, which can be used for your reference.
+### getSensorOut
+Odpowiada za zliczanie pikseli w 3 częściach obrazu (część górna, środkowa oraz dolna). Dron zostanie wysterowany w zależności od tego w których częściach obrazu zliczy odpowiednią ilość białych pikseli. Obiektem wyjściowym jest tablica 1x3 zawierająca wartości 0 lub 1.
 
-- **Tello_Video(With_Pose_Recognition)**
+1 - ścieżka
 
- Tello_Video_With_Pose_Recognition is an application version modified from Tello_Video.It uses the decoded video data，and everytime extract a single frame image for pose recognition operation , and binds the specific posture and aircraft control commands to realize the pose control of Tello.This code is mainly used as an application case for utilizing the decoded video data of tello for image processing.
+0 - brak ścieżki
 
-- **Tello_state.py**
 
- Tello_state.py can read the various status data of tello, and can be used as a tool to debug and view the status of tello.
+### distance_control
+Odpowiada za nadanie prędkości drona w zależności od tego w jakiej odległości znajduje się od ściany.
 
-## Environmental configuration
+### send_commands
+Na podstawie tablicy wartości funkcji getSensorOut oraz distance_control nadawane są odpowiednie prędkości góra/dół oraz przód/tył, aby dron utrzymał się na wytyczonej ścieżce.
 
-The sample codes above are based on python2.7.There is no need to install additional third-party libraries for running Single_Tello_Test and tello_state.py.For Tello_Video and Tello_Video (With_Pose_Recognition), you need to install a series of third-party libraries. Therefore, in these two folders, a one-click installation script (based on windows32/64, linux and macos) is provided, which can facilitate you with installing all relevant dependencies.
 
-Specific to the content and description of each package, you can refer to the readme file in the related folder. 
- 
-## Contact Information
+## Dodatkowe pliki
 
-If you have any questions about this sample code and the installation, please feel free to contact me. You can communicate with me by sending e-mail to sdk@ryzerobotics.com.
-And recently we have committed a new FAQ file under the 'Tello-Python'.If you have any questions,you can firstly refer to it .
+### video_mod.py
+Moduł pozwalający na nagrywanie obrazu, który widzi drona.
 
-## About Multi-Tello-Formation
-
-Please refer to github repository https://github.com/TelloSDK/Multi-Tello-Formation.
-This is a python program that enable the function of multi-tello swarms. 
+### color_setter.py
+Moduł pozwalający na ręczne dobranie wartości hsv do progowania ścieżki oraz znaczników odległości.
 
 
 
--- Tello_Video/line_follower.py
+++ Tello_Video/line_follower.py
@@ -5,30 +5,34 @@ import time
 from video_mod import VideoOut
 
 
+# Inicjalizacja drona
 drone = tello.Tello()
 drone.connect()
 print('Battery: ', drone.get_battery())
 drone.streamon()
 
+# Ustawienie nagrywania
 vid = VideoOut("out3", 'mp4', 480, 360, 30)
-hsv_Vals_red_line = [0, 160, 11, 179, 255, 255]
 
+# Wartosci do progowania
+hsv_Vals_red_line = [0, 160, 11, 179, 255, 255]
 hsv_Vals_markers = [20, 99, 180, 40, 255, 255]
+
+# Wartosci do kontroli odleglosci od sciany
 distance_control_minV = 40
 distance_control_maxV = 80
 
-
-num_sensors = 3
-th_value = 0.1
+num_sensors = 3  # podzial obrazu
+th_value = 0.1  # mnoznik wartosci bialych pixeli
 width, height = 480, 360
-sensitivity = 3
-weights = [20, 15, 0, -20, -25]
-up_down = 0
-lSpeed = 20
-repeat = False
-fb = 0
+weights = [20, 15, 0, -20, -25]  # wartosci sterowania (gora/dol)
+up_down = 0  # wartosc sterowania (gora/dol)
+lSpeed = 20  # predkosc drona
+repeat = False  # zmienna pomocnicza
+fb = 0  # wartosc sterowania (przod/tyl)
 
 
+# progowanie sciezki
 def threshold(img, thresh):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     lower = np.array([thresh[0], thresh[1], thresh[2]])
@@ -38,6 +42,7 @@ def threshold(img, thresh):
     return mask
 
 
+# wykrywanie konturow do sterowania odlegloscia
 def contours(img_th, img):
     cnt, _ = cv2.findContours(img_th, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
     cv2.drawContours(img, cnt, -1, (255, 255, 0), 7)
@@ -54,7 +59,7 @@ def contours(img_th, img):
         cy = y + h//2
         cv2.drawContours(img, biggest, -1, (255, 255, 0), 7)
         cv2.circle(img, (cx, cy), 10, (0, 255, 0), cv2.FILLED)
-        if w >= 25 and h >= 25:
+        if w >= 25 and h >= 25:  # sprawdzenie wielkosci znacznikow
             return w, h
         else:
             return 0, 0
@@ -62,18 +67,22 @@ def contours(img_th, img):
         return 0, 0
 
 
+# informacja o pixelach na 3 czesciach obrazu
 def getSensorOut(imgThresh, num_sensors):
-    # horizontal
+    # podzielenie obrazu na 5 czesci, aby zawezic pole decyzyjne drona
     img_copy = imgThresh.copy()
     imgs_horizontal = np.hsplit(img_copy, 5)
+
+    # wybor tylko 3 srodkowych obrazow
     central_imgs = [imgs_horizontal[1], imgs_horizontal[2], imgs_horizontal[3]]
     final_img = cv2.hconcat(central_imgs)
 
-    #vertical
+    # podzielenie obrazu na 3 czesci wertykalnie, aby moc sterowac dronem gora/dol
     imgs = np.vsplit(final_img, num_sensors)
     pixels = (final_img.shape[1]//num_sensors) * final_img.shape[0]
     sensor_out = []
 
+    # sprawdzanie bialych pixeli w obrazie
     for num, im in enumerate(imgs):
         cnt_pixels = cv2.countNonZero(im)
         if cnt_pixels > th_value*pixels:
@@ -81,11 +90,11 @@ def getSensorOut(imgThresh, num_sensors):
         else:
             sensor_out.append(0)
         cv2.imshow(str(num), im)
-    # print(sensor_out)
 
     return sensor_out
 
 
+# kontrola odleglosci od sciany
 def distance_control(m_width, m_height, min_val, max_val):
     global fb
     param = max([m_width, m_height])
@@ -101,6 +110,9 @@ def distance_control(m_width, m_height, min_val, max_val):
         fb = -20
 
 
+# wysylanie komend do drona
+# w zaleznosci od wartosci 'sensor_out' czyli informacji, gdzie są biale pixele (sciezka),
+# wysylamy odpowiednie sterowanie
 def send_commands(sensor_out):
     global up_down, repeat, fb
 
@@ -118,13 +130,23 @@ def send_commands(sensor_out):
     elif sensor_out == [0, 0, 1]:
         up_down = weights[4]
 
+    # jesli na obrazie nie ma bialych pixeli i repeat==False
+    # to sprawdzamy poprzednia komedne
     elif sensor_out == [0, 0, 0] and repeat is False:
         fb = 0
         repeat = True
+
+        # jesli dron mial leciec w gore, to zmieniamy aby lecial w dol,
+        # poniewaz prawdopodobnie polecial za wysoko i zgubil trase
         if up_down > 0:
             up_down = weights[3]
+
+        # jesli dron mial leciec w dol, to zmieniamy aby lecial w gore - analogiczna sytuacja
         elif up_down < 0:
             up_down = weights[1]
+
+    # jesli na obrazie nie ma bialych pixeli i repeat==True
+    # to wykonujemy komende z poprzedniego if'a
     elif sensor_out == [0, 0, 0] and repeat is True:
         fb = 0
 
@@ -149,20 +171,23 @@ while True:
         drone.takeoff()
         time.sleep(1)
 
-    imgThresh = threshold(img, hsv_Vals_red_line)
-    markerImage = threshold(img, hsv_Vals_markers)
+    imgThresh = threshold(img, hsv_Vals_red_line)  # progowanie sciezki
+    markerImage = threshold(img, hsv_Vals_markers)  # progowanie znacznikow odleglosci
 
-    sensor_out = getSensorOut(imgThresh, num_sensors)
+    sensor_out = getSensorOut(imgThresh, num_sensors)  # informacja o pixelach na obrazie (0/1)
 
-    marker_width, marker_height = contours(markerImage, img)
-    distance_control(marker_height, marker_width, distance_control_minV, distance_control_maxV)
-    send_commands(sensor_out)
+    marker_width, marker_height = contours(markerImage, img)  # kontury znacznikow
+    distance_control(marker_height, marker_width, distance_control_minV, distance_control_maxV)  # kontrola odleglosci od sciany
+    send_commands(sensor_out)  # wysylanie komend do drona
 
-    # record video
+    # nagrywanie video
     vid.write_frame(img_to_show)
 
     cv2.imshow('Img', img)
-    # cv2.imshow('Img_thresh', imgThresh)
+
+    # przerwanie symulacji klawiszem 'q' - zakonczenie nagrywania oraz ladowanie
+
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
